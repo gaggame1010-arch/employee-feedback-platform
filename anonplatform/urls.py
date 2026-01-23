1b21bf6ee6ab7e3e873ffda7b1d86f6d39833bd1
gaggame1010-arch <gaggame1010@gmail.com>
2026-01-23 13:25:28 +0400

    Add favicon handler to prevent 404 warnings

--- anonplatform/urls.py
+++ anonplatform/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
+
+
+def favicon_view(request):
+    """Return empty response for favicon requests to avoid 404 warnings."""
+    return HttpResponse(status=204)  # No Content
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('favicon.ico', favicon_view, name='favicon'),
     path("", include("submissions.urls")),
 ]
